f874f938dc5e36195850a696c2d4c61139eda791
Thiago Tomaz <thiagotomaz.nc@gmail.com>
2026-07-20 15:05:43 -0300

    correção do bug do tema escuro

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.thiago.tomaz.calculaifrn"
         minSdk = 24
         targetSdk = 36
-        versionCode = 14
+        versionCode = 15
         versionName = "3.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
